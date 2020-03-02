dd26460afe43d0a7f1a198694aa5f272b843a041
Ampex <kasperski.blazej@gmail.com>
2020-03-02 21:06:36 +0100

    Updates

---
+++ precache-manifest.af2a24167e198fe3e40cc0cf3abe5b9c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5285c0bdaf20534d905d3cf038e7bcda",
+    "url": "/jobTask/index.html"
+  },
+  {
+    "revision": "3734fc5cb10f239fb2fd",
+    "url": "/jobTask/static/css/main.6bcf11cd.chunk.css"
+  },
+  {
+    "revision": "a044683a26c49594d0b1",
+    "url": "/jobTask/static/js/2.0fbea7d0.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/jobTask/static/js/2.0fbea7d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3734fc5cb10f239fb2fd",
+    "url": "/jobTask/static/js/main.9fad20f4.chunk.js"
+  },
+  {
+    "revision": "32015a5b2391d138efc8",
+    "url": "/jobTask/static/js/runtime-main.97dac98f.js"
+  },
+  {
+    "revision": "b6547c29fb6e3eacbce7a213208a2999",
+    "url": "/jobTask/static/media/a.b6547c29.jpg"
+  },
+  {
+    "revision": "1a2893a4d4f133d6e622bb75bc797865",
+    "url": "/jobTask/static/media/b.1a2893a4.jpg"
+  },
+  {
+    "revision": "3e7bb0e948575c2b72f073b8d94e4012",
+    "url": "/jobTask/static/media/c.3e7bb0e9.jpg"
+  },
+  {
+    "revision": "dbbb7ca833fb7e320fca80b12d42a874",
+    "url": "/jobTask/static/media/d.dbbb7ca8.jpg"
+  },
+  {
+    "revision": "af35e1dfcab34de3ab2f6615fee005c3",
+    "url": "/jobTask/static/media/e.af35e1df.jpg"
+  },
+  {
+    "revision": "a519adeb442e28cd0cbb16d828a743f8",
+    "url": "/jobTask/static/media/f.a519adeb.jpg"
+  },
+  {
+    "revision": "10602be3cc0d965911983f6f5ae16582",
+    "url": "/jobTask/static/media/g.10602be3.jpg"
+  },
+  {
+    "revision": "55fa4a137726be299d03305e16a2352a",
+    "url": "/jobTask/static/media/h.55fa4a13.jpg"
+  },
+  {
+    "revision": "c52594ddeabf6376ce90d7ab3beda2fd",
+    "url": "/jobTask/static/media/i.c52594dd.jpg"
+  },
+  {
+    "revision": "36d06b743125d639b66b00ca134013d0",
+    "url": "/jobTask/static/media/logo.36d06b74.svg"
+  }
+]);
